6c3b4573ea3054686b0f9328aa2381d98972d336
Ganesh Nayanajith <32520964+ganeshnayanajith@users.noreply.github.com>
2023-07-02 17:17:21 +0530

    Library and Header component basic

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -6,6 +6,7 @@ import { HiHome } from "react-icons/hi";
 import { BiSearch } from "react-icons/bi";
 import Box from "@/components/Box";
 import SidebarItem from "@/components/SidebarItem";
+import Library from "@/components/Library";
 
 interface SidebarProps {
   children: React.ReactNode;
@@ -44,9 +45,12 @@ const Sidebar: React.FC<SidebarProps> = ({ children }) => {
           </div>
         </Box>
         <Box className="overflow-y-auto h-full">
-          Song Library
+          <Library/>
         </Box>
       </div>
+      <main className="h-full flex-1 overflow-y-auto py-2">
+        {children}
+      </main>
     </div>
   );
 };
